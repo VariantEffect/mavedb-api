b7ef30e062cd02c28308e9a72bf2f6c6ee35fa4a
Alan Rubin <alan.rubin@wehi.edu.au>
2023-03-30 16:48:06 +1100

    update validation error messsages

--- tests/view_models/test_wild_type_sequence.py
+++ tests/view_models/test_wild_type_sequence.py
@@ -14,18 +14,18 @@ def test_create_invalid_sequence_type(test_empty_db):
     sequence = "ATGAGTATTCAACATTTCCGTGTC"
     with pytest.raises(ValueError) as exc_info:
         WildTypeSequenceCreate(sequence_type=sequence_type, sequence=sequence)
-    assert "dnaaa is not a valid sequence type. Valid sequence types are infer, dna, and protein" in str(exc_info.value)
+    assert f"'{sequence_type}' is not a valid sequence type" in str(exc_info.value)
 
 def test_create_not_match_sequence_and_seq_type(test_empty_db):
     sequence_type = "dna"
     sequence = "ARCG"
     with pytest.raises(ValueError) as exc_info:
         WildTypeSequenceCreate(sequence_type=sequence_type, sequence=sequence)
-    assert "sequence type does not match sequence_type. sequence type is protein, while sequence_type is dna." in str(exc_info.value)
+    assert "invalid protein sequence provided" in str(exc_info.value)
 
 def test_create_not_match_sequence_and_seq_type(test_empty_db):
     sequence_type = "protein"
     sequence = "AzCG"
     with pytest.raises(ValueError) as exc_info:
         WildTypeSequenceCreate(sequence_type=sequence_type, sequence=sequence)
-    assert "sequence is invalid. It is not a correct target sequence." in str(exc_info.value)
+    assert "invalid protein sequence provided" in str(exc_info.value)
