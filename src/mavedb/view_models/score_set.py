fdd9cf3374251dbfaa634bbfa056c7db67dddd9b
Jeremy Stone <74574922+jstone-uw@users.noreply.github.com>
2023-06-15 18:12:52 -0700

    Prevent KeyError exceptions when validating meta analysis experiment URNs.
    These shouldn't arise anyway, but this avoids problems in case of some strange behavior in converting properties from camelCase to snake_case.

--- src/mavedb/view_models/score_set.py
+++ src/mavedb/view_models/score_set.py
@@ -82,8 +82,8 @@ class ScoreSetCreate(ScoreSetModify):
 
     @root_validator
     def validate_experiment_urn_required_except_for_meta_analyses(cls, values):
-        experiment_urn = values["experiment_urn"]
-        meta_analysis_source_score_set_urns = values["meta_analysis_source_score_set_urns"]
+        experiment_urn = values.get("experiment_urn")
+        meta_analysis_source_score_set_urns = values.get("meta_analysis_source_score_set_urns")
         is_meta_analysis = meta_analysis_source_score_set_urns is None or len(meta_analysis_source_score_set_urns) == 0
         if experiment_urn is None and is_meta_analysis:
             raise ValidationError("An experiment URN is required, unless your score set is a meta-analysis.")
