f4c7764155b08b3a9eeda38a74c7d95e79f816d8
Alan Rubin <alan.rubin@wehi.edu.au>
2023-06-22 16:04:10 +1000

    first test for publication

--- tests/routers/test_score_set.py
+++ tests/routers/test_score_set.py
@@ -38,7 +38,6 @@ def test_create_minimal_score_set(test_score_set_db):
         assert expected_response[key] == response_data[key]
     response = client.get(f"/api/v1/score-sets/{response_data['urn']}")
     assert response.status_code == 200
-    print(json.dumps(response.json(), indent=2))
 
 
 def test_get_own_private_score_set(test_score_set_db):
@@ -69,6 +68,20 @@ def test_get_other_user_private_score_set(test_score_set_db):
     change_ownership(score_set_urn, ScoreSetDbModel)
     response = client.get(f"/api/v1/score-sets/{score_set_urn}")
     assert response.status_code == 404
+    response_data = response.json()
+    assert f"score set with URN '{score_set_urn}' not found" in response_data["detail"]
+
+
+def test_cannot_publish_score_set_without_variants(test_score_set_db):
+    experiment = test_score_set_db
+    score_set_post_payload = deepcopy(TEST_MINIMAL_SCORE_SET)
+    score_set_post_payload["experimentUrn"] = experiment["urn"]
+    response = client.post("/api/v1/score-sets/", json=score_set_post_payload)
+    response_data = response.json()
+    score_set_urn = response_data["urn"]
+    response = client.post(f"/api/v1/score-sets/{score_set_urn}/publish", json=score_set_post_payload)
+    assert response.status_code == 422
+    assert f"cannot publish score set without variant scores" in response_data["detail"]
 
 
 def test_create_score_set_with_some_none_values(test_empty_db):
