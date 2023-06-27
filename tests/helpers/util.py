a53eda6c518431a225269b16908fddd3a1f79a09
Alan Rubin <alan.rubin@wehi.edu.au>
2023-06-27 11:08:03 +1000

    add extra validation checks to creation utilities

--- tests/helpers/util.py
+++ tests/helpers/util.py
@@ -2,8 +2,8 @@ import jsonschema
 from copy import deepcopy
 
 from mavedb.models.user import User
-from mavedb.view_models.experiment import ExperimentCreate
-from mavedb.view_models.score_set import ScoreSetCreate
+from mavedb.view_models.experiment import Experiment, ExperimentCreate
+from mavedb.view_models.score_set import ScoreSet, ScoreSetCreate
 from tests.helpers.constants import EXTRA_USER, TEST_MINIMAL_EXPERIMENT, TEST_MINIMAL_SCORE_SET
 
 
@@ -26,7 +26,9 @@ def create_experiment(client, update=None):
     jsonschema.validate(instance=experiment_payload, schema=ExperimentCreate.schema())
     response = client.post("/api/v1/experiments/", json=experiment_payload)
     assert response.status_code == 200
-    return response.json()
+    response_data = response.json()
+    jsonschema.validate(instance=response_data, schema=Experiment.schema())
+    return response_data
 
 
 def create_score_set(client, experiment_urn, update=None):
@@ -38,7 +40,9 @@ def create_score_set(client, experiment_urn, update=None):
     jsonschema.validate(instance=score_set_payload, schema=ScoreSetCreate.schema())
     response = client.post("/api/v1/score-sets/", json=score_set_payload)
     assert response.status_code == 200
-    return response.json()
+    response_data = response.json()
+    jsonschema.validate(instance=response_data, schema=ScoreSet.schema())
+    return response_data
 
 
 def create_score_set_with_variants(client, experiment_urn, scores_csv_path, update=None, counts_csv_path=None):
@@ -51,8 +55,12 @@ def create_score_set_with_variants(client, experiment_urn, scores_csv_path, upda
     else:
         counts_file = None
     response = client.post(f"/api/v1/score-sets/{score_set['urn']}/variants/data", files=files)
-    assert response.status_code == 200
+
     score_file.close()
     if counts_file is not None:
         counts_file.close()
-    return response.json()
+
+    assert response.status_code == 200
+    response_data = response.json()
+    jsonschema.validate(instance=response_data, schema=ScoreSet.schema())
+    return response_data
