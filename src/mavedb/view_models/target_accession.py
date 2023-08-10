61baca27b5d95fed02c264921bc50f00928a882b
Ben Capodanno <capodb@uw.edu>
2023-08-10 12:28:07 -0700

    Add New View Model for Target Accession
    Adds the associated view model for the new `target_accession` model.

---
+++ src/mavedb/view_models/target_accession.py
@@ -0,0 +1,38 @@
+from datetime import date
+
+from mavedb.view_models.base.base import BaseModel
+
+
+class TargetAccessionBase(BaseModel):
+    accession: str
+
+
+class TargetAccessionModify(TargetAccessionBase):
+    # Consider some validation, ie: Accession is in our SeqRepo instance
+    pass
+
+
+class TargetAccessionCreate(TargetAccessionModify):
+    pass
+
+
+class TargetAccessionUpdate(TargetAccessionModify):
+    pass
+
+
+# Properties shared by models stored in DB
+class SavedTargetAccession(TargetAccessionBase):
+    class Config:
+        orm_mode = True
+        arbitrary_types_allowed = True
+
+
+# Properties to return to non-admin clients
+class TargetAccession(SavedTargetAccession):
+    pass
+
+
+# Properties to return to admin clients
+class AdminTargetAccession(SavedTargetAccession):
+    creation_date: date
+    modification_date: date
