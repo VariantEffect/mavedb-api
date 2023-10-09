439e79eb607c81f0e7ec1ab1eada921557576cdb
Ben Capodanno <capodb@uw.edu>
2023-10-09 12:11:20 -0700

    Add Gene Accession Routes
    Adds some routes for manipulating gene based accession data from our
    seqrepo instance.

--- src/mavedb/routers/hgvs.py
+++ src/mavedb/routers/hgvs.py
@@ -1,13 +1,14 @@
+from itertools import chain
 from typing import Any, Union
 
-import hgvs.validator
-import hgvs.parser
-import hgvs.dataproviders.uta
-import hgvs.dataproviders.seqfetcher
-import hgvs.dataproviders.interface
-from fastapi import APIRouter, HTTPException
-import hgvs
 import bioutils.assemblies
+import hgvs
+import hgvs.dataproviders.interface
+import hgvs.dataproviders.seqfetcher
+import hgvs.dataproviders.uta
+import hgvs.parser
+import hgvs.validator
+from fastapi import APIRouter, HTTPException
 
 router = APIRouter(
     prefix="/api/v1/hgvs",
@@ -16,16 +17,16 @@ router = APIRouter(
 )
 
 
-@router.get("/fetch/{seq_id}", status_code=200, response_model=Any)
-def hgvs_fetch(seq_id: str) -> Any:
+@router.get("/fetch/{accession}", status_code=200, response_model=Any)
+def hgvs_fetch(accession: str) -> Any:
     """
     List stored sequences
     """
-    return hgvs.dataproviders.uta.connect().seqfetcher.fetch_seq(seq_id)
+    return hgvs.dataproviders.uta.connect().seqfetcher.fetch_seq(accession)
 
 
 @router.post("/validate", status_code=200, response_model=Any)
-def hgvs_validate(variant: str) -> Any:
+def hgvs_validate(variant: dict[str, str]) -> Any:
     """
     List stored sequences
     """
@@ -33,7 +34,7 @@ def hgvs_validate(variant: str) -> Any:
     hdp = hgvs.dataproviders.uta.connect()
 
     try:
-        valid = hgvs.validator.Validator(hdp=hdp).validate(hp.parse(variant), strict=False)
+        valid = hgvs.validator.Validator(hdp=hdp).validate(hp.parse(variant["variant"]), strict=False)
     except hgvs.validator.HGVSInvalidVariantError as e:
         raise HTTPException(400, str(e))
     else:
@@ -69,9 +70,38 @@ def list_assemblies_grouped() -> Any:
     ]
 
 
-@router.get("/{assembly}/accessions", status_code=200, response_model=list[str])
+@router.get("/{assembly}/accessions", status_code=200, response_model=list)
 def list_accessions(assembly: str) -> Any:
     """
     List stored accessions
     """
     return list(hgvs.dataproviders.uta.connect().get_assembly_map(assembly_name=assembly).keys())
+
+
+@router.get("/genes", status_code=200, response_model=list)
+def list_genes():
+    return list(chain.from_iterable(hgvs.dataproviders.uta.connect()._fetchall("select hgnc from gene")))
+
+
+@router.get("/genes/{gene}", status_code=200, response_model=list)
+def gene_info(gene: str):
+    return hgvs.dataproviders.uta.connect().get_gene_info(gene)
+
+
+@router.get("/transcripts/gene/{gene}", status_code=200, response_model=list)
+def list_transcripts_for_gene(gene: str):
+    # the 4th element of each tx_info list is the accession number for the transcript. Only
+    # surface unique elements
+    return set([tx_info[3] for tx_info in hgvs.dataproviders.uta.connect().get_tx_for_gene(gene)])
+
+
+@router.get("/transcripts/{transcript}", status_code=200, response_model=list)
+def transcript_info(transcript: str):
+    # the 4th element of each tx_info list is the accession number for the transcript. Only
+    # surface unique elements
+    return hgvs.dataproviders.uta.connect().get_tx_identity_info(transcript)
+
+
+@router.get("/transcripts/protein/{transcript}", status_code=200, response_model=str)
+def convert_to_protein(transcript: str):
+    return hgvs.dataproviders.uta.connect().get_pro_ac_for_tx_ac(transcript)
