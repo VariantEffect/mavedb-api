a176d41570882da0aedcad38015a0b3d86644567
Nick Moore <nick@zoic.org>
2023-12-21 12:08:16 +1100

    add reference to TargetGeneCreate and TargetSequenceCreate tests

--- tests/view_models/test_wild_type_sequence.py
+++ tests/view_models/test_wild_type_sequence.py
@@ -1,7 +1,17 @@
 from mavedb.view_models.target_sequence import TargetSequenceCreate
+from mavedb.models.reference_genome import ReferenceGenome
 
 import pytest
 
+import datetime
+
+reference_genome = ReferenceGenome(
+    id = 1,
+    short_name = "Name",
+    organism_name = "Organism",
+    creation_date = datetime.datetime.now(),
+    modification_date = datetime.datetime.now(),
+)
 
 @pytest.mark.parametrize(
     "sequence_type, sequence",
@@ -13,7 +23,7 @@ import pytest
     ],
 )
 def test_create_wild_type_sequence(client, sequence_type, sequence):
-    TargetSeq = TargetSequenceCreate(sequence_type=sequence_type, sequence=sequence)
+    TargetSeq = TargetSequenceCreate(sequence_type=sequence_type, sequence=sequence, reference=reference_genome)
     assert TargetSeq.sequence_type == sequence_type.lower()
     assert TargetSeq.sequence == sequence.upper()
 
@@ -21,12 +31,12 @@ def test_create_wild_type_sequence(client, sequence_type, sequence):
 @pytest.mark.parametrize("sequence_type, sequence", [("dnaaa", "ATGAGTATTCAACATTTCCGTGTC"), ("null", "STARTREK")])
 def test_create_invalid_sequence_type(client, sequence_type, sequence):
     with pytest.raises(ValueError) as exc_info:
-        TargetSequenceCreate(sequence_type=sequence_type, sequence=sequence)
+        TargetSequenceCreate(sequence_type=sequence_type, sequence=sequence, reference=reference_genome)
     assert f"'{sequence_type}' is not a valid sequence type" in str(exc_info.value)
 
 
 @pytest.mark.parametrize("sequence_type, sequence", [("dna", "ARCG"), ("protein", "AzCG")])
 def test_create_invalid_sequence(client, sequence_type, sequence):
     with pytest.raises(ValueError) as exc_info:
-        TargetSequenceCreate(sequence_type=sequence_type, sequence=sequence)
+        TargetSequenceCreate(sequence_type=sequence_type, sequence=sequence, reference=reference_genome)
     assert f"invalid {sequence_type} sequence provided" in str(exc_info.value)
