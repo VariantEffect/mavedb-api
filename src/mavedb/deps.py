1735ab50c3430cfb727398ff84dfb3b9b566891f
Ben Capodanno <capodb@uw.edu>
2024-01-11 11:28:19 -0800

    Use CDOT to Resolve Transcripts when not Present in UTA
    Uses the built-in `ChainedSeqFetcher` provided with CDOT to resolve transcripts that are potentially
    missing from UTA. This is surfaced as a new Dep to ensure that all instances of our genomic coordinate
    validators and seqfetchers use the same data pool. If a transcript is present in UTA, we will prefer
    that sequence. If a transcript is not present in UTA, we will use the transcripts associated with the
    human genome builds GRCh38 and GRCh37.

--- src/mavedb/deps.py
+++ src/mavedb/deps.py
@@ -2,6 +2,7 @@
 import sys
 from typing import Generator
 
+from cdot.hgvs.dataproviders import RESTDataProvider, ChainedSeqFetcher, FastaSeqFetcher, SeqFetcher
 from sqlalchemy.dialects.postgresql import JSONB as POSTGRES_JSONB
 from sqlalchemy.types import JSON
 
@@ -15,6 +16,16 @@ def get_db() -> Generator:
         yield db
     finally:
         db.close()
+
+
+def hgvs_data_provider() -> Generator:
+    grch38_fetcher = FastaSeqFetcher("/data/GCF_000001405.39_GRCh38.p13_genomic.fna.gz")
+    grch37_fetcher = FastaSeqFetcher("/data/GCF_000001405.25_GRCh37.p13_genomic.fna.gz")
+
+    # Prioritize fetching from SeqRepo, then GRCh38, then GRCh37.
+    seqfetcher = ChainedSeqFetcher(SeqFetcher(), grch38_fetcher, grch37_fetcher)
+
+    yield RESTDataProvider(seqfetcher=seqfetcher)
 
 
 # if 'PYTEST_RUN_CONFIG' in os.environ:
