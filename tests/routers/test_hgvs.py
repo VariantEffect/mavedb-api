d2636d3f6dc771528b58ec164f9d2e5eee85818d
Nick Moore <nick@zoic.org>
2024-01-19 14:24:15 +1100

    run more tests on hgvs endpoints

--- tests/routers/test_hgvs.py
+++ tests/routers/test_hgvs.py
@@ -2,13 +2,43 @@ VALID_MAJOR_ASSEMBLY = "GRCh38"
 VALID_MINOR_ASSEMBLY = "GRCh38.p3"
 INVALID_ASSEMBLY = "undefined"
 VALID_ACCESSION = "NC_000001.11"
+INVALID_ACCESSION = "NC_999999.99"
+SMALL_ACCESSION = "NM_002977.4"
 VALID_GENE = "BRCA1"
 INVALID_GENE = "fnord"
 VALID_TRANSCRIPT = 'NM_001408458.1'
 INVALID_TRANSCRIPT = 'NX_99999.1'
+VALID_VARIANT = VALID_ACCESSION + ":g.1_2insA"
+INVALID_VARIANT = VALID_ACCESSION + ":g.1delA"
+HAS_PROTEIN_ACCESSION = "NM_000014.4"
+PROTEIN_ACCESSION = "NP_000005.2"
 
 
-def test_hgvs_assemblies(client, setup_router_db):
+def test_hgvs_fetch_valid(client, setup_router_db):
+    # VALID_ACCESSION is rather large so use a smaller one
+    response = client.get(f"/api/v1/hgvs/fetch/{SMALL_ACCESSION}")
+    assert response.status_code == 200
+
+
+def test_hgvs_fetch_invalid(client, setup_router_db):
+    response = client.get(f"/api/v1/hgvs/fetch/{INVALID_ACCESSION}")
+    assert response.status_code == 404
+
+
+def test_hgvs_validate_valid(client, setup_router_db):
+    payload = {'variant': VALID_VARIANT}
+    response = client.post("/api/v1/hgvs/validate", json=payload)
+    assert response.status_code == 200
+
+
+def test_hgvs_validate_invalid(client, setup_router_db):
+    payload = {'variant': INVALID_VARIANT}
+    response = client.post("/api/v1/hgvs/validate", json=payload)
+    assert response.status_code == 400
+    assert 'does not agree' in response.json()['detail']
+
+
+def test_hgvs_list_assemblies(client, setup_router_db):
     response = client.get("/api/v1/hgvs/assemblies")
     assert response.status_code == 200
     assert VALID_MAJOR_ASSEMBLY in response.json()
@@ -51,6 +81,8 @@ def test_hgvs_genes(client, setup_router_db):
 def test_hgvs_gene_info_valid(client, setup_router_db):
     response = client.get(f"/api/v1/hgvs/genes/{VALID_GENE}")
     assert response.status_code == 200
+    assert response.json()['hgnc'] == VALID_GENE
+    assert response.json()['descr'] is not None
 
 
 def test_hgvs_gene_info_invalid(client, setup_router_db):
@@ -90,13 +122,24 @@ def test_hgvs_transcript_invalid(client, setup_router_db):
 
 
 def test_hgvs_transcript_protein_valid(client, setup_router_db):
-    response = client.get(f"/api/v1/hgvs/transcripts/protein/{VALID_TRANSCRIPT}")
-    # XXX I have no idea what this is supposed to return
+    response = client.get(f"/api/v1/hgvs/transcripts/protein/{HAS_PROTEIN_ACCESSION}")
     assert response.status_code == 200
+    assert response.json() == PROTEIN_ACCESSION
 
 
-def test_hgvs_transcript_protein_invalid(client, setup_router_db):
-    response = client.get(f"/api/v1/hgvs/transcripts/protein/{INVALID_TRANSCRIPT}")
+def test_hgvs_transcript_protein_no_protein(client, setup_router_db):
+    response = client.get(f"/api/v1/hgvs/transcripts/protein/{SMALL_ACCESSION}")
     # XXX this probably SHOULD return a 404, but currently returns a 200 with None
     # assert response.status_code == 404
     assert response.status_code == 200
+    assert response.json() is None
+
+
+def test_hgvs_transcript_protein_invalid(client, setup_router_db):
+    response = client.get(f"/api/v1/hgvs/transcripts/protein/{INVALID_ACCESSION}")
+    # XXX this probably SHOULD return a 400, but currently returns a 200 with None
+    # assert response.status_code == 404
+    assert response.status_code == 200
+    assert response.json() is None
+
+
