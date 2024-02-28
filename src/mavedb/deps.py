39bf3819336490d02c3a10393daa9e1deb17bb44
Ben Capodanno <capodb@uw.edu>
2024-02-28 10:57:39 -0800

    Use HGVS Data Provider Directly on HGVS Routes
    Transitions to using the HGVS dependency data provider on most HGVS routes (the one exception is
    the `/genes` route, of which UTA provides a more complete list). This change eliminates all
    references to and usage of SeqRepo based methods within the HGVS router.
    
    This change also alters router logic so that empty or NoneType responses that would have surfaced a
    200 response + an empty data field now respond with a 404 error. Tests have been edited to reflect
    this change.

--- src/mavedb/deps.py
+++ src/mavedb/deps.py
@@ -3,7 +3,6 @@
 from typing import Generator
 
 from cdot.hgvs.dataproviders import RESTDataProvider, ChainedSeqFetcher, FastaSeqFetcher
-from hgvs.dataproviders.seqfetcher import SeqFetcher
 from sqlalchemy.dialects.postgresql import JSONB
 
 from mavedb.db.session import SessionLocal
@@ -19,23 +18,9 @@ def get_db() -> Generator:
 
 
 def hgvs_data_provider() -> RESTDataProvider:
-    # Prioritize fetching from SeqRepo, then GRCh38, then GRCh37.
-    seqfetcher = ChainedSeqFetcher(SeqFetcher())
-
-    # Attempt to resolve FASTA Seq fetchers from data files, but don't fail if neither file is
-    # available. This way, we at least retain some ability to resolve sequences if we don't have
-    # FASTA file access and we are able to run our test suite without needing access to large genomic
-    # files.
-    try:
-        grch38_fetcher = FastaSeqFetcher("/data/GCF_000001405.39_GRCh38.p13_genomic.fna.gz")
-        seqfetcher.seq_fetchers.append(grch38_fetcher)
-    except OSError:
-        pass
-
-    try:
-        grch37_fetcher = FastaSeqFetcher("/data/GCF_000001405.25_GRCh37.p13_genomic.fna.gz")
-        seqfetcher.seq_fetchers.append(grch37_fetcher)
-    except OSError:
-        pass
-
+    # Prioritize fetching from GRCh38, then GRCh37.
+    seqfetcher = ChainedSeqFetcher(
+        FastaSeqFetcher("/data/GCF_000001405.39_GRCh38.p13_genomic.fna.gz"),
+        FastaSeqFetcher("/data/GCF_000001405.25_GRCh37.p13_genomic.fna.gz"),
+    )
     return RESTDataProvider(seqfetcher=seqfetcher)
