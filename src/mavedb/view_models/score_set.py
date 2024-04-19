2582af8f6364ae54000d71bfc00bc9ffd765b605
Ben Capodanno <capodb@uw.edu>
2024-04-19 16:45:01 -0700

    Fix Target Label Validator Bug
    Fixes a bug that caused the target label validator to not enter its validation logic
    and miss potentially duplicated label values.

--- src/mavedb/view_models/score_set.py
+++ src/mavedb/view_models/score_set.py
@@ -88,7 +88,7 @@ class ScoreSetModify(ScoreSetBase):
     @validator("target_genes")
     def target_labels_are_unique(cls, field_value, values):
         # Labels are only used on target sequence instances.
-        if len(field_value) > 1 and all([isinstance(target, TargetSequence) for target in field_value]):
+        if len(field_value) > 1 and all([target.target_sequence is not None for target in field_value]):
             labels = [target.target_sequence.label for target in field_value]
             dup_indices = [idx for idx, item in enumerate(labels) if item in labels[:idx]]
             if dup_indices:
