149cb79a61c3c5951dd6da214f6a740e0a08a2da
Jeremy Stone <74574922+jstone-uw@users.noreply.github.com>
2024-04-29 12:55:38 -0700

    Filter out score sets not licensed under CC0, and include a CC0 license in the archive.

--- src/mavedb/scripts/export_public_data.py
+++ src/mavedb/scripts/export_public_data.py
@@ -20,14 +20,15 @@ Experiments are nested in their parent experiment sets, and score sets in their 
 The variant URNs used in filenames do not include the `urn:mavedb:` scheme identifier, so they look like
 `00000001-a-1.counts.csv` and `00000001-a-1.scores.csv`, for instance.
 
-Unpublished data are not included in the dump, and user details are limited to ORCID IDs and names of contributors to
-published data sets.
+Unpublished data and data sets licensed other than under the Create Commmons Zero license are not included in the dump,
+and user details are limited to ORCID IDs and names of contributors to published data sets.
 """
 
 from datetime import datetime, timezone
 from itertools import chain
 import json
 import logging
+import os
 from typing import Callable, Iterable, TypeVar
 from zipfile import ZipFile
 
@@ -39,6 +40,7 @@ from mavedb.lib.score_sets import get_score_set_counts_as_csv, get_score_set_sco
 from mavedb.lib.script_environment import init_script_environment
 from mavedb.models.experiment import Experiment
 from mavedb.models.experiment_set import ExperimentSet
+from mavedb.models.license import License
 from mavedb.models.score_set import ScoreSet
 from mavedb.view_models.experiment_set import ExperimentSetPublicDump
 
@@ -50,6 +52,39 @@ S = TypeVar("S")
 T = TypeVar("T")
 
 
+def filter_experiment_sets(experiment_sets: Iterable[ExperimentSet]) -> Iterable[ExperimentSet]:
+    """
+    Filter a list of experiment sets. Exclude any experiments with no score sets, then exclude experiment sets with no
+    experiments.
+
+    Filtering is done on the basis of the current contents of Experiment.score_set, which will have been loaded using a
+    query that excludes unpublished score sets and those licensed other than under CC0.
+    """
+    return filter(filter_experiment_set, experiment_sets)
+
+
+def filter_experiment_set(experiment_set: ExperimentSet):
+    """
+    Filter an experiment set. Exclude any experiments it contains that do not contain score sets, and return a value
+    indicating whether any experiments remain.
+
+    Filtering is done on the basis of the current contents of Experiment.score_set, which will have been loaded using a
+    query that excludes unpublished score sets and those licensed other than under CC0.
+    """
+    experiment_set.experiments = list(filter_experiments(experiment_set.experiments))
+    return len(experiment_set.experiments) > 0
+
+
+def filter_experiments(experiments: Iterable[Experiment]) -> Iterable[Experiment]:
+    """
+    Filter a list of experiments, excluding any whose score_sets collection is empty.
+
+    Filtering is done on the basis of the current contents of score_sets, which will have been loaded using a query that
+    excludes unpublished score sets and those licensed other than under CC0.
+    """
+    return filter(lambda e: len(e.score_sets) > 0, experiments)
+
+
 def flatmap(f: Callable[[S], Iterable[T]], items: Iterable[S]) -> Iterable[T]:
     return chain.from_iterable(map(f, items))
 
@@ -59,23 +94,30 @@ experiment_sets_query = db.scalars(
     .where(ExperimentSet.published_date.is_not(None))
     .options(
         lazyload(ExperimentSet.experiments.and_(Experiment.published_date.is_not(None))).options(
-            lazyload(Experiment.score_sets.and_(ScoreSet.published_date.is_not(None)))
+            lazyload(
+                Experiment.score_sets.and_(
+                    ScoreSet.published_date.is_not(None), ScoreSet.license.has(License.short_name == "CC0")
+                )
+            )
         )
     )
     .execution_options(populate_existing=True)
     .order_by(ExperimentSet.urn)
 )
 
+# Filter the stream of experiment sets to exclude experiments and experiment sets with no public, CC0-licensed score
+# sets.
+experiment_sets = filter_experiment_sets(experiment_sets_query.all())
+
 # TODO To support very large data sets, we may want to use custom code for JSON-encoding an iterator.
 # Issue: https://github.com/VariantEffect/mavedb-api/issues/192
 # See, for instance, https://stackoverflow.com/questions/12670395/json-encoding-very-long-iterators.
 
-experiment_sets = experiment_sets_query.all()
 experiment_set_views = map(lambda es: ExperimentSetPublicDump.from_orm(es), experiment_sets)
 
 # Get a list of IDS of all the score sets included. (There is no flat_map function...)
-score_set_ids = flatmap(
-    lambda es: flatmap(lambda e: map(lambda ss: ss.id, e.score_sets), es.experiments), experiment_sets
+score_set_ids = list(
+    flatmap(lambda es: flatmap(lambda e: map(lambda ss: ss.id, e.score_sets), es.experiments), experiment_sets)
 )
 
 zip_file_name = "mavedb-dump.zip"
@@ -88,7 +130,13 @@ json_data = {
 }
 
 with ZipFile(zip_file_name, "w") as zipfile:
+    # Write metadata for all data sets to a single JSON file.
     zipfile.writestr("main.json", json.dumps(jsonable_encoder(json_data)))
+
+    # Copy the CC0 license.
+    zipfile.write(os.path.join(os.path.dirname(__file__), "resources/CC0_license.txt"), "LICENSE.txt")
+
+    # Write score and count files for each score set.
     for score_set_id in score_set_ids:
         score_set = db.scalars(select(ScoreSet).where(ScoreSet.id == score_set_id)).one_or_none()
         if score_set is not None and score_set.urn is not None:
