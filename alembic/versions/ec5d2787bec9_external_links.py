f706623987896462b8865cf16f1c13213343b0d4
Jeremy Stone <74574922+jstone-uw@users.noreply.github.com>
2024-05-30 08:16:20 -0700

    Nicer Alembic migration for non-nullable JSONB column

--- alembic/versions/ec5d2787bec9_external_links.py
+++ alembic/versions/ec5d2787bec9_external_links.py
@@ -20,15 +20,8 @@ depends_on = None
 
 
 def upgrade():
-    op.add_column("scoresets", sa.Column("external_links", postgresql.JSONB(astext_type=sa.Text()), nullable=True))
+    op.add_column("scoresets", sa.Column("external_links", postgresql.JSONB(astext_type=sa.Text()), nullable=False, server_default='{}'))
 
-    # Update all score sets, setting external_links to the default value.
-    conn = op.get_bind()
-    session = Session(bind=conn)
-    session.execute(ScoreSet.__table__.update().values(external_links = {}))
-    #session.commit()
-
-    #op.alter_column("scoresets", "external_links", nullable=False)
 
 def downgrade():
     op.drop_column("scoresets", "external_links")
