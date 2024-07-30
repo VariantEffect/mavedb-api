fb1b5dee9767d5dd9070053e64422cda43cb4fec
Ben Capodanno <capodb@uw.edu>
2024-07-29 17:06:41 -0700

    Use Starlette Context to Manage Log Context

--- src/mavedb/lib/logging/canonical.py
+++ src/mavedb/lib/logging/canonical.py
@@ -1,15 +1,16 @@
-import json
 import logging
 import os
+from typing import Any
 
 import boto3
 from arq import ArqRedis
 from arq.jobs import Job
-from fastapi.requests import Request
-from fastapi.responses import Response
 from watchtower import CloudWatchLogHandler
 
-from mavedb.lib.logging.models import WorkerRecord, APIRecord, Source, LogType
+from mavedb import __version__
+from mavedb.lib.logging.models import Source, LogType
+from mavedb.lib.logging.context import save_to_context, dump_context
+
 
 logger = logging.getLogger(__name__)
 
@@ -21,59 +22,43 @@ if AWS_REGION_NAME and CLOUDWATCH_LOG_GROUP:
     logger.addHandler(CloudWatchLogHandler(boto3_client=boto3_logs_client, log_group_name=CLOUDWATCH_LOG_GROUP))
 
 else:
-    logger.warning("Canonical CloudWatch Handler is not defined. Canonical logs will not be sent to CloudWatch.")
-
-FRONTEND_URL = os.getenv("FRONTEND_URL", "")
-API_URL = os.getenv("API_URL", "")
+    logger.warning("Canonical CloudWatch Handler is not defined. Canonical logs will only be emitted to stderr.")
 
 
 async def log_job(ctx: dict):
     redis: ArqRedis = ctx["redis"]
+    job_id = ctx["job_id"]
 
-    completed_job = Job(ctx["job_id"], redis=redis)
+    completed_job = Job(job_id, redis=redis)
+    log_context: dict[str, Any] = ctx["state"].pop(job_id) if job_id in ctx["state"] else {}
     result = await completed_job.result_info()
 
     if not result:
-        logger.warning(f"Job finished, but could not retrieve a job result for job {ctx['job_id']}")
+        logger.warning(f"Job finished, but could not retrieve a job result for job {job_id}")
 
     assert result
-    record: WorkerRecord = {
-        "id": completed_job.job_id,
-        "log_type": LogType.worker_job,
-        "source": Source.worker,
-        "time_ns": int(result.enqueue_time.timestamp()),
-        "queued_ns": (result.start_time - result.enqueue_time).microseconds,
-        "duration_ns": (result.finish_time - result.start_time).microseconds,
-        "job": result.function,
-        "attempt": result.job_try,
-        "success": result.success,
-        "result": result.result,
-    }
+    save_to_context(
+        {
+            "job_id": completed_job.job_id,
+            "version": __version__,
+            "log_type": LogType.worker_job,
+            "source": Source.worker,
+            "time_ns": int(result.enqueue_time.timestamp()),
+            "queued_ns": (result.start_time - result.enqueue_time).microseconds,
+            "duration_ns": (result.finish_time - result.start_time).microseconds,
+            "job_name": result.function,
+            "job_attempt": result.job_try,
+            "process_result": result.success,
+            "job_result": result.result,
+            **log_context,
+        }
+    )
 
     if not result.success:
-        logger.warning(json.dumps(record))
+        logger.warning(f"Job completed unsuccessfully. {dump_context()}")
     else:
-        logger.info(json.dumps(record))
+        logger.info(f"Job completed successfully. {dump_context()}")
 
 
-def log_request(request: Request, response: Response, start: int, end: int):
-    source = Source.other
-    if request.headers.get("origin") == FRONTEND_URL:
-        source = Source.web
-    elif request.headers.get("referer") == API_URL + "/docs":
-        source = Source.docs
-
-    user_agent = request.headers.get("user-agent", "None")
-
-    record: APIRecord = {
-        "log_type": LogType.api_request,
-        "source": source,
-        "time_ns": start,
-        "duration_ns": end - start,
-        "path": request.url.path,
-        "method": request.method,
-        "response_code": response.status_code,
-        "user_agent": user_agent,
-        **request.state._state,
-    }
-    logger.info(json.dumps(record))
+def log_request():
+    logger.info(f"Request completed. {dump_context()}")
