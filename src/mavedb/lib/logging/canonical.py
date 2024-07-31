7e6c95cc6a7c7f137d793ea9541fcf415e7f9749
Ben Capodanno <capodb@uw.edu>
2024-07-31 16:39:25 -0700

    Make Canonical Log Dump Fully JSON

--- src/mavedb/lib/logging/canonical.py
+++ src/mavedb/lib/logging/canonical.py
@@ -20,7 +20,6 @@ AWS_REGION_NAME = os.getenv("AWS_REGION_NAME", "")
 if AWS_REGION_NAME and CLOUDWATCH_LOG_GROUP:
     boto3_logs_client = boto3.client("logs", region_name=AWS_REGION_NAME)
     logger.addHandler(CloudWatchLogHandler(boto3_client=boto3_logs_client, log_group_name=CLOUDWATCH_LOG_GROUP))
-
 else:
     logger.warning("Canonical CloudWatch Handler is not defined. Canonical logs will only be emitted to stderr.")
 
@@ -29,12 +28,14 @@ async def log_job(ctx: dict):
     redis: ArqRedis = ctx["redis"]
     job_id = ctx["job_id"]
 
-    completed_job = Job(job_id, redis=redis)
     log_context: dict[str, Any] = ctx["state"].pop(job_id) if job_id in ctx["state"] else {}
+    save_to_context(log_context)
+
+    completed_job = Job(job_id, redis=redis)
     result = await completed_job.result_info()
 
     if not result:
-        logger.warning(f"Job finished, but could not retrieve a job result for job {job_id}")
+        logger.warning(f"Job finished, but could not retrieve a job result for job {job_id}. {dump_context()}")
 
     assert result
     save_to_context(
@@ -50,15 +51,17 @@ async def log_job(ctx: dict):
             "job_attempt": result.job_try,
             "process_result": result.success,
             "job_result": result.result,
-            **log_context,
         }
     )
 
     if not result.success:
-        logger.warning(f"Job completed unsuccessfully. {dump_context()}")
+        save_to_context({"message": "Job completed unsuccessfully."})
+        logger.warning(dump_context())
     else:
-        logger.info(f"Job completed successfully. {dump_context()}")
+        save_to_context({"message": "Job completed successfully."})
+        logger.info(dump_context())
 
 
 def log_request():
-    logger.info(f"Request completed. {dump_context()}")
+    save_to_context({"message": "Request comleted."})
+    logger.info(dump_context())
