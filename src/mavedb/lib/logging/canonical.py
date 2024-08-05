5ebcba8e00227c2b4e92d93c0a2eb6a3ab4e98bb
Ben Capodanno <capodb@uw.edu>
2024-08-05 10:37:38 -0700

    Use Locally Managed Context for Worker Jobs

--- src/mavedb/lib/logging/canonical.py
+++ src/mavedb/lib/logging/canonical.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import json
 from typing import Any
 
 import boto3
@@ -26,42 +27,60 @@ else:
     logger.warning("Canonical CloudWatch Handler is not defined. Canonical logs will only be emitted to stderr.")
 
 
+# NOTE: Starlette context will not be initialized in the worker, so maintain a local dictionary of context. We
+#       may eventually want to harden this by using the backing Redis cache or with Python's contextvars.
 async def log_job(ctx: dict):
     redis: ArqRedis = ctx["redis"]
     job_id = ctx["job_id"]
 
     log_context: dict[str, Any] = ctx["state"].pop(job_id) if job_id in ctx["state"] else {}
-    save_to_context(log_context)
 
     completed_job = Job(job_id, redis=redis)
     result = await completed_job.result_info()
 
     if not result:
-        logger.warning(dump_context(message=f"Job finished, but could not retrieve a job result for job {job_id}."))
+        log_context["message"] = f"Job finished, but could not retrieve a job result for job {job_id}."
+        logger.warning(json.dumps(log_context))
+        log_context.pop("message")
+    else:
+        log_context = {
+            **log_context,
+            **{
+                "time_ns": int(result.enqueue_time.timestamp()),
+                "queued_ns": (result.start_time - result.enqueue_time).microseconds,
+                "duration_ns": (result.finish_time - result.start_time).microseconds,
+                "job_name": result.function,
+                "job_attempt": result.job_try,
+                "process_result": result.success,
+                "job_result": result.result,
+            },
+        }
 
-    assert result
-    save_to_context(
-        {
+    log_context = {
+        **log_context,
+        **{
             "job_id": completed_job.job_id,
             "version": __version__,
             "log_type": LogType.worker_job,
             "source": Source.worker,
-            "time_ns": int(result.enqueue_time.timestamp()),
-            "queued_ns": (result.start_time - result.enqueue_time).microseconds,
-            "duration_ns": (result.finish_time - result.start_time).microseconds,
-            "job_name": result.function,
-            "job_attempt": result.job_try,
-            "process_result": result.success,
-            "job_result": result.result,
-        }
-    )
+        },
+    }
 
-    if result.result == "success":
-        logger.info(dump_context(message="Job completed successfully."))
+    if result is None:
+        log_context["message"] = "Job result coul not be found."
+        logger.error(json.dumps(log_context))
+    elif result.result == "success":
+        log_context["message"] = "Job completed successfully."
+        logger.info(json.dumps(log_context))
     elif result.result != "success":
-        logger.warning(dump_context(message="Job completed with handled exception."))
+        log_context["message"] = "Job completed with handled exception."
+        logger.warning(json.dumps(log_context))
     else:
-        logger.error(dump_context(message="Job completed with unhandled exception."))
+        log_context["message"] = "Job completed with unhandled exception."
+        logger.error(json.dumps(log_context))
+
+    log_context.pop("message")
+    flush_cloudwatch_logs()
 
 
 def log_request(request: Request, response: Response, start: int, end: int):
@@ -80,3 +99,11 @@ def log_request(request: Request, response: Response, start: int, end: int):
         logger.warning(dump_context(message="Request comleted."))
     else:
         logger.error(dump_context(message="Request comleted with exception."))
+
+
+def flush_cloudwatch_logs():
+    """Force flush of all cloudwatch logging handlers. For example at the end of a process just before it is killed."""
+    root_logger = logging.getLogger()
+    for handler in root_logger.handlers:
+        if isinstance(handler, CloudWatchLogHandler):
+            handler.flush()
