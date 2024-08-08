f3b7d7df6ae504b96a6538d7f27b50fbbb18eecb
EstelleDa <baishenlonger@live.com.sg>
2024-08-08 15:43:16 +1000

    Add more tests. Find a bug in creating a new experiment.

---
+++ tests/view_models/test_experiment.py
@@ -0,0 +1,75 @@
+import pytest
+from fastapi.encoders import jsonable_encoder
+
+from mavedb.view_models.experiment import ExperimentCreate
+
+
+def test_cannot_create_experiment_without_a_title():
+    invalid_experiment = {
+        "shortDescription": "Test experiment",
+        "abstractText": "Abstract",
+        "methodText": "Methods",
+    }
+
+    with pytest.raises(ValueError) as exc_info:
+        ExperimentCreate(**jsonable_encoder(invalid_experiment))
+
+    assert "field required" in str(exc_info.value)
+    assert "title" in str(exc_info.value)
+
+
+def test_cannot_create_experiment_with_a_space_title():
+    invalid_experiment = {
+        "title": " ",
+        "shortDescription": "Test experiment",
+        "abstractText": "Abstract",
+        "methodText": "Methods",
+    }
+
+    with pytest.raises(ValueError) as exc_info:
+        ExperimentCreate(**jsonable_encoder(invalid_experiment))
+
+    assert "field required" in str(exc_info.value)
+    assert "title" in str(exc_info.value)
+
+
+def test_cannot_create_experiment_without_a_short_description():
+    invalid_experiment = {
+        "title": "title",
+        "abstractText": "Abstract",
+        "methodText": "Methods",
+    }
+
+    with pytest.raises(ValueError) as exc_info:
+        ExperimentCreate(**jsonable_encoder(invalid_experiment))
+
+    assert "field required" in str(exc_info.value)
+    assert "shortDescription" in str(exc_info.value)
+
+
+def test_cannot_create_experiment_without_an_abstract():
+    invalid_experiment = {
+        "title": "title",
+        "shortDescription": "Test experiment",
+        "methodText": "Methods",
+    }
+
+    with pytest.raises(ValueError) as exc_info:
+        ExperimentCreate(**jsonable_encoder(invalid_experiment))
+
+    assert "field required" in str(exc_info.value)
+    assert "abstractText" in str(exc_info.value)
+
+
+def test_cannot_create_experiment_without_a_method():
+    invalid_experiment = {
+        "title": "title",
+        "shortDescription": "Test experiment",
+        "abstractText": "Abstract",
+    }
+
+    with pytest.raises(ValueError) as exc_info:
+        ExperimentCreate(**jsonable_encoder(invalid_experiment))
+
+    assert "field required" in str(exc_info.value)
+    assert "methodText" in str(exc_info.value)
