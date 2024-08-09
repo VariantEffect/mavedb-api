63be7ebef2368d225ea49c7a2811d852d913d6c7
EstelleDa <baishenlonger@live.com.sg>
2024-08-09 10:44:17 +1000

    Add more tests for experiment view model, but need validators in experiment.

--- tests/view_models/test_experiment.py
+++ tests/view_models/test_experiment.py
@@ -29,7 +29,22 @@ def test_cannot_create_experiment_with_a_space_title():
     with pytest.raises(ValueError) as exc_info:
         ExperimentCreate(**jsonable_encoder(invalid_experiment))
 
-    assert "field required" in str(exc_info.value)
+    assert "Invalid title. Title should not be None or space." in str(exc_info.value)
+    assert "title" in str(exc_info.value)
+
+
+def test_cannot_create_experiment_with_an_empty_title():
+    invalid_experiment = {
+        "title": "",
+        "shortDescription": "Test experiment",
+        "abstractText": "Abstract",
+        "methodText": "Methods",
+    }
+
+    with pytest.raises(ValueError) as exc_info:
+        ExperimentCreate(**jsonable_encoder(invalid_experiment))
+
+    assert "none is not an allowed value" in str(exc_info.value)
     assert "title" in str(exc_info.value)
 
 
@@ -47,6 +62,36 @@ def test_cannot_create_experiment_without_a_short_description():
     assert "shortDescription" in str(exc_info.value)
 
 
+def test_cannot_create_experiment_with_a_space_short_description():
+    invalid_experiment = {
+        "title": "title",
+        "shortDescription": " ",
+        "abstractText": "Abstract",
+        "methodText": "Methods",
+    }
+
+    with pytest.raises(ValueError) as exc_info:
+        ExperimentCreate(**jsonable_encoder(invalid_experiment))
+
+    assert "Invalid short description. Short description should not be None or space" in str(exc_info.value)
+    assert "shortDescription" in str(exc_info.value)
+
+
+def test_cannot_create_experiment_with_an_empty_short_description():
+    invalid_experiment = {
+        "title": "title",
+        "shortDescription": "",
+        "abstractText": "Abstract",
+        "methodText": "Methods",
+    }
+
+    with pytest.raises(ValueError) as exc_info:
+        ExperimentCreate(**jsonable_encoder(invalid_experiment))
+
+    assert "none is not an allowed value" in str(exc_info.value)
+    assert "shortDescription" in str(exc_info.value)
+
+
 def test_cannot_create_experiment_without_an_abstract():
     invalid_experiment = {
         "title": "title",
@@ -61,6 +106,36 @@ def test_cannot_create_experiment_without_an_abstract():
     assert "abstractText" in str(exc_info.value)
 
 
+def test_cannot_create_experiment_with_a_space_abstract():
+    invalid_experiment = {
+        "title": "title",
+        "shortDescription": "Test experiment",
+        "abstractText": " ",
+        "methodText": "Methods",
+    }
+
+    with pytest.raises(ValueError) as exc_info:
+        ExperimentCreate(**jsonable_encoder(invalid_experiment))
+
+    assert "Invalid abstract text. Abstract text should not be None or space." in str(exc_info.value)
+    assert "abstractText" in str(exc_info.value)
+
+
+def test_cannot_create_experiment_with_an_empty_abstract():
+    invalid_experiment = {
+        "title": "title",
+        "shortDescription": "Test experiment",
+        "abstractText": "",
+        "methodText": "Methods",
+    }
+
+    with pytest.raises(ValueError) as exc_info:
+        ExperimentCreate(**jsonable_encoder(invalid_experiment))
+
+    assert "none is not an allowed value" in str(exc_info.value)
+    assert "abstractText" in str(exc_info.value)
+
+
 def test_cannot_create_experiment_without_a_method():
     invalid_experiment = {
         "title": "title",
@@ -72,4 +147,34 @@ def test_cannot_create_experiment_without_a_method():
         ExperimentCreate(**jsonable_encoder(invalid_experiment))
 
     assert "field required" in str(exc_info.value)
+    assert "methodText" in str(exc_info.value)
+
+
+def test_cannot_create_experiment_with_a_space_method():
+    invalid_experiment = {
+        "title": "title",
+        "shortDescription": "Test experiment",
+        "abstractText": "Abstract",
+        "methodText": " ",
+    }
+
+    with pytest.raises(ValueError) as exc_info:
+        ExperimentCreate(**jsonable_encoder(invalid_experiment))
+
+    assert "Invalid method text. Method text should not be None or space." in str(exc_info.value)
+    assert "methodText" in str(exc_info.value)
+
+
+def test_cannot_create_experiment_with_an_empty_method():
+    invalid_experiment = {
+        "title": "title",
+        "shortDescription": "Test experiment",
+        "abstractText": "Abstract",
+        "methodText": "",
+    }
+
+    with pytest.raises(ValueError) as exc_info:
+        ExperimentCreate(**jsonable_encoder(invalid_experiment))
+
+    assert "none is not an allowed value" in str(exc_info.value)
     assert "methodText" in str(exc_info.value)
