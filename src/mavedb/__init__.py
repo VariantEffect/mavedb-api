86aac8927ebe95717c722b48cfa649a9c2ddf8d3
Ben Capodanno <capodb@uw.edu>
2024-08-13 09:04:16 -0700

    Application Version on Startup

--- src/mavedb/__init__.py
+++ src/mavedb/__init__.py
@@ -1,6 +1,11 @@
-import mavedb.logging
+import logging as module_logging
 
-mavedb.logging.configure()
+import mavedb.logging as application_logging
+
+application_logging.configure()
+logger = module_logging.getLogger(__name__)
 
 __project__ = "mavedb-api"
 __version__ = "2024.2.2"
+
+logger.info(f"MaveDB {__version__}")
