33e31af29ea29e551352365000308c4586b8c2e3
Ben Capodanno <capodb@uw.edu>
2025-02-11 10:02:23 -0800

    VA-Spec tests for contributions and datasets

--- tests/lib/conftest.py
+++ tests/lib/conftest.py
@@ -6,10 +6,13 @@ from datetime import datetime
 
 from mavedb.models.enums.user_role import UserRole
 from mavedb.models.experiment_set import ExperimentSet
+from mavedb.models.experiment import Experiment
 from mavedb.models.license import License
 from mavedb.models.role import Role
 from mavedb.models.taxonomy import Taxonomy
+from mavedb.models.score_set import ScoreSet
 from mavedb.models.user import User
+from mavedb.models.variant import Variant
 from mavedb.models.mapped_variant import MappedVariant
 from tests.helpers.constants import (
     ADMIN_USER,
@@ -18,6 +21,9 @@ from tests.helpers.constants import (
     TEST_INACTIVE_LICENSE,
     TEST_SAVED_TAXONOMY,
     TEST_USER,
+    VALID_SCORE_SET_URN,
+    VALID_EXPERIMENT_URN,
+    VALID_EXPERIMENT_SET_URN,
 )
 
 
@@ -59,8 +65,41 @@ def mock_mapped_variant():
 
 
 @pytest.fixture
-def mock_resource():
+def mock_variant():
+    variant = mock.Mock(spec=Variant)
+    variant.urn = f"{VALID_SCORE_SET_URN}#1"
+    variant.score_set.urn = VALID_SCORE_SET_URN
+    return variant
+
+
+@pytest.fixture
+def mock_experiment_set():
     resource = mock.Mock(spec=ExperimentSet)
+    resource.urn = VALID_EXPERIMENT_SET_URN
     resource.creation_date = datetime(2023, 1, 1)
     resource.modification_date = datetime(2023, 1, 2)
     return resource
+
+
+@pytest.fixture
+def mock_experiment():
+    experiment = mock.Mock(spec=Experiment)
+    experiment.title = "Test Experiment"
+    experiment.urn = VALID_EXPERIMENT_URN
+    experiment.creation_date = datetime(2023, 1, 1)
+    experiment.modification_date = datetime(2023, 1, 2)
+    return experiment
+
+
+@pytest.fixture
+def mock_score_set(mock_user):
+    score_set = mock.Mock(spec=ScoreSet)
+    score_set.urn = VALID_SCORE_SET_URN
+    score_set.license.short_name = "MIT"
+    score_set.created_by = mock_user
+    score_set.modified_by = mock_user
+    score_set.published_date = datetime(2023, 1, 1)
+    score_set.title = "Mock score set"
+    score_set.creation_date = datetime(2023, 1, 2)
+    score_set.modification_date = datetime(2023, 1, 3)
+    return score_set
