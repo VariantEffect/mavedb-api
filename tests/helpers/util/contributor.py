1be90dad1f33a90025347a08218712aa04ea0af4
Ben Capodanno <capodb@uw.edu>
2025-02-12 16:41:01 -0800

    MyPy fixes for bumped dependency type regressions

--- tests/helpers/util/contributor.py
+++ tests/helpers/util/contributor.py
@@ -3,6 +3,7 @@ from sqlalchemy import select
 from sqlalchemy.orm import Session
 from typing import Any
 
+from mavedb.lib.exceptions import NonexistentOrcidUserError
 from mavedb.models.contributor import Contributor
 
 
@@ -20,3 +21,7 @@ def add_contributor(db: Session, urn: str, model: Any, orcid_id: str, given_name
     item.contributors = [contributor]
     db.add(item)
     db.commit()
+
+
+def callable_nonexistent_orcid_user_exception():
+    raise NonexistentOrcidUserError()
