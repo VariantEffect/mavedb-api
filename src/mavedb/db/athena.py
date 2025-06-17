71719aaa6086c12fae0ba00706ef889b2ea96fe5
Ben Capodanno <capodb@uw.edu>
2025-06-17 08:43:40 -0700

    Add gnomAD joint frequency data model (Athena based) and table model loading function

--- src/mavedb/db/athena.py
+++ src/mavedb/db/athena.py
@@ -8,6 +8,8 @@ ATHENA_S3_STAGING_DIR = os.getenv("ATHENA_S3_STAGING_DIR")
 
 ATHENA_URL = "awsathena+rest://:@athena.{region_name}.amazonaws.com:443/{schema_name}?s3_staging_dir={s3_staging_dir}"
 
+GNOMAD_JOINT_FREQUENCY_TABLE_NAME = os.getenv("GNOMAD_JOINT_FREQUENCY_TABLE_NAME", "mavedb2_gnomad_data")
+
 engine = create_engine(
     ATHENA_URL.format(
         region_name=AWS_REGION_NAME,
