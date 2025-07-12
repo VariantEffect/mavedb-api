a93ae5e01a3cb2afa4e30813538408bf37798a9f
Ben Capodanno <capodb@uw.edu>
2025-07-12 10:16:07 -0700

    Refactor migration script to remove references to container ranges and use evidence strength directly

--- alembic/manual_migrations/migrate_consolidated_score_ranges_and_calibrations.py
+++ alembic/manual_migrations/migrate_consolidated_score_ranges_and_calibrations.py
@@ -9,7 +9,7 @@ from mavedb.models import *
 from sqlalchemy.orm import Session
 
 from mavedb.models.score_set import ScoreSet
-from mavedb.view_models.score_range import ScoreSetRangesCreate, RangeContainerCreate, CalibrationContainerCreate, InvestigatorScoreRangesCreate, PillarProjectScoreRangesCreate, PillarProjectScoreRangeCreate
+from mavedb.view_models.score_range import ScoreSetRangesCreate, InvestigatorScoreRangesCreate, PillarProjectScoreRangesCreate, PillarProjectScoreRangeCreate
 
 
 from mavedb.db.session import SessionLocal
@@ -17,16 +17,28 @@ from mavedb.db.session import SessionLocal
 configure_mappers()
 
 
+evidence_strength_to_label = {
+    -8: "BS3_VERY_STRONG",
+    -4: "BS3_STRONG",
+    -3: "BS3_MODERATE+",
+    -2: "BS3_MODERATE",
+    -1: "BS3_SUPPORTING",
+    1: "PS3_SUPPORTING",
+    2: "PS3_MODERATE",
+    3: "PS3_MODERATE+",
+    4: "PS3_STRONG",
+    8: "PS3_VERY_STRONG",
+}
+
+
 def do_migration(db: Session):
     score_sets_with_ranges_or_calibrations = db.scalars(sa.select(ScoreSet).where(ScoreSet.score_ranges.isnot(None) | ScoreSet.score_calibrations.isnot(None))).all()
 
     for score_set in score_sets_with_ranges_or_calibrations:
         if score_set.score_ranges is not None:
-            range_container = RangeContainerCreate(
-                investigator_provided=InvestigatorScoreRangesCreate(**score_set.score_ranges),
-            )
+            investigator_ranges = InvestigatorScoreRangesCreate(**score_set.score_ranges)
         else:
-            range_container = None
+            investigator_ranges = None
 
         if score_set.score_calibrations is not None:
             thresholds = score_set.score_calibrations.get("pillar_project", {}).get("thresholds", [])
@@ -46,6 +58,7 @@ def do_migration(db: Session):
                         range=(None, threshold),
                         classification="normal" if evidence_strength < 0 else "abnormal",
                         label=str(evidence_strength),
+                        evidence_strength=evidence_strength,
                         positive_likelihood_ratio=positive_likelihood_ratio,
                     ))
                 elif idx == len(thresholds) - 1:
@@ -53,6 +66,7 @@ def do_migration(db: Session):
                     ranges.append(PillarProjectScoreRangeCreate(
                         range=(threshold, None),
                         classification="normal" if evidence_strength < 0 else "abnormal",
+                        evidence_strength=evidence_strength,
                         label=str(evidence_strength),
                         positive_likelihood_ratio=positive_likelihood_ratio,
                     ))
@@ -66,6 +80,7 @@ def do_migration(db: Session):
                         range=calculated_range,
                         classification="normal" if evidence_strength < 0 else "abnormal",
                         label=str(evidence_strength),
+                        evidence_strength=evidence_strength,
                         positive_likelihood_ratio=positive_likelihood_ratio,
                     ))
 
@@ -73,19 +88,17 @@ def do_migration(db: Session):
                 if idx != len(evidence_strengths) - 1 and (evidence_strengths[idx + 1] * evidence_strength < 0):
                     boundary_direction = -boundary_direction
 
-            calibration_container = CalibrationContainerCreate(
-                pillar_project=PillarProjectScoreRangesCreate(
+                pillar_project_ranges = PillarProjectScoreRangesCreate(
                     prior_probability_pathogenicity=prior_probability_pathogenicity,
                     parameter_sets=parameter_sets,
                     ranges=ranges,
-                ),
-            )
+                )
         else:
-            calibration_container = None
+            pillar_project_ranges = None
 
         score_set.score_ranges = ScoreSetRangesCreate(
-            ranges=range_container,
-            calibrations=calibration_container,
+            investigator_provided=investigator_ranges if investigator_ranges else None,
+            pillar_project=pillar_project_ranges if pillar_project_ranges else None,
         ).dict()
         db.add(score_set)
 
