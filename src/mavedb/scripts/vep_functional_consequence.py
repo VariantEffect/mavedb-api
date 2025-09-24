2f86e1b12a530bc19d5633319369da860221b307
Sally Grindstaff <sallybg@uw.edu>
2025-09-24 14:52:59 -0700

    Batch initial requests to VEP

--- src/mavedb/scripts/vep_functional_consequence.py
+++ src/mavedb/scripts/vep_functional_consequence.py
@@ -64,22 +64,40 @@ VEP_CONSEQUENCES = [
 ]
 
 
-def get_functional_consequence(hgvs_string: str) -> Optional[str]:
+def get_functional_consequence(hgvs_strings: Sequence[str]) -> dict[str, Optional[str]]:
     headers = {"Content-Type": "application/json", "Accept": "application/json"}
-    response = requests.get(f"{ENSEMBL_API_URL}/vep/human/hgvs/{hgvs_string}", headers=headers)
-    # Nonsense and synonymous protein variants can't be processed directly by VEP, need to be run through Variant Recoder first
-    if response.status_code == 400:
-        logger.warning(
-            f"VEP API could not process HGVS string {hgvs_string}: {response.text}. Retrieving genomic variant(s) from Variant Recoder."
-        )
+    result: dict[str, Optional[str]] = {}
+
+    # Batch POST to VEP
+    response = requests.post(
+        f"{ENSEMBL_API_URL}/vep/human/hgvs",
+        headers=headers,
+        json={"hgvs_notations": hgvs_strings},
+    )
+
+    missing_hgvs = set(hgvs_strings)
+    if response.status_code == 200:
+        data = response.json()
+        # Map HGVS to consequence
+        for entry in data:
+            hgvs = entry.get("input")
+            most_severe_consequence = entry.get("most_severe_consequence")
+            if hgvs:
+                result[hgvs] = most_severe_consequence
+                missing_hgvs.discard(hgvs)
+    else:
+        logger.error(f"Failed batch VEP API request: {response.status_code} {response.text}")
+
+    # Fallback for missing HGVS strings
+    for hgvs_string in missing_hgvs:
+        # Variant Recoder fallback
         variant_recoder_response = requests.get(
             f"{ENSEMBL_API_URL}/variant_recoder/human/{hgvs_string}", headers=headers
         )
         if variant_recoder_response.status_code != 200:
-            logger.error(
-                f"Failed to query Variant Recoder API for {hgvs_string}: {variant_recoder_response.status_code}"
-            )
-            return None
+            logger.error(f"Failed to query Variant Recoder for {hgvs_string}: {variant_recoder_response.status_code}")
+            result[hgvs_string] = None
+            continue
         recoder_data = variant_recoder_response.json()
         if isinstance(recoder_data, list) and len(recoder_data) == 1:
             variants = []
@@ -89,51 +107,47 @@ def get_functional_consequence(hgvs_string: str) -> Optional[str]:
                     if genomic_hgvs.startswith("NC_"):
                         variants.append(genomic_hgvs)
             if not variants:
-                logger.error(f"No genomic HGVS strings found in Variant Recoder response for {hgvs_string}.")
-                return None
+                logger.error(f"No genomic HGVS found in Variant Recoder response for {hgvs_string}.")
+                result[hgvs_string] = None
+                continue
             consequences = []
             for genomic_hgvs in variants:
                 vep_response = requests.get(f"{ENSEMBL_API_URL}/vep/human/hgvs/{genomic_hgvs}", headers=headers)
                 if vep_response.status_code != 200:
-                    logger.error(f"Failed to query VEP API for {genomic_hgvs}: {vep_response.status_code}")
+                    logger.error(f"Failed VEP for {genomic_hgvs}: {vep_response.status_code}")
                     # if any of the genomic variants fail, return None, because we don't want to miss the most severe consequence
-                    return None
+                    result[hgvs_string] = None
+                    break
                 vep_data = vep_response.json()
                 if isinstance(vep_data, list) and len(vep_data) == 1:
                     most_severe_consequence = vep_data[0].get("most_severe_consequence")
                     if most_severe_consequence:
                         consequences.append(most_severe_consequence)
                     else:
-                        logger.error(f"No most_severe_consequence found in VEP response for {genomic_hgvs}.")
+                        logger.error(f"No consequence returned from VEP for {genomic_hgvs}")
                         # if any of the genomic variants fail, return None, because we don't want to miss the most severe consequence
-                        return None
+                        result[hgvs_string] = None
+                        break
                 else:
-                    logger.error(f"Unexpected response format from VEP API for {genomic_hgvs}: {vep_data}")
-                    return None
+                    logger.error(f"Unexpected VEP format for {genomic_hgvs}: {vep_data}")
+                    result[hgvs_string] = None
+                    break
             if consequences:
                 # Return the most severe consequence among all genomic variants
                 for consequence in VEP_CONSEQUENCES:
                     if consequence in consequences:
-                        return consequence
+                        result[hgvs_string] = consequence
+                        break
+                else:
+                    result[hgvs_string] = None
             else:
                 logger.error(f"No consequences found for any genomic variants derived from {hgvs_string}.")
-                return None
+                result[hgvs_string] = None
         else:
-            logger.error(f"Unexpected response format from Variant Recoder API for {hgvs_string}: {recoder_data}")
-            return None
+            logger.error(f"Unexpected Variant Recoder format for {hgvs_string}: {recoder_data}")
+            result[hgvs_string] = None
 
-    elif response.status_code != 200:
-        logger.error(f"Failed to query VEP API for {hgvs_string}: {response.status_code}")
-        return None
-
-    data = response.json()
-    if isinstance(data, list) and len(data) == 1:
-        most_severe_consequence = data[0].get("most_severe_consequence")
-        return most_severe_consequence
-    else:
-        logger.error(f"Unexpected response format from VEP API for {hgvs_string}: {data}")
-
-    return None
+    return result
 
 
 @script_environment.command()
@@ -176,19 +190,43 @@ def populate_functional_consequences(db: Session, urns: Sequence[Optional[str]],
             if not mapped_variants:
                 logger.info(f"No mapped variant post-mapped objects found for score set {score_set.urn}.")
                 continue
+
+            queue = []
+            variant_map = {}
             for mapped_variant in mapped_variants:
                 hgvs_string = mapped_variant.post_mapped.get("expressions", {})[0].get("value")  # type: ignore
                 if not hgvs_string:
                     logger.warning(f"No HGVS string found in post_mapped for variant {mapped_variant.id}.")
                     continue
-                functional_consequence = get_functional_consequence(hgvs_string)
-                if functional_consequence:
-                    mapped_variant.vep_functional_consequence = functional_consequence
-                    mapped_variant.vep_access_date = date.today()
-                    db.add(mapped_variant)
+                queue.append(hgvs_string)
+                variant_map[hgvs_string] = mapped_variant
+
+                if len(queue) == 200:
+                    consequences = get_functional_consequence(queue)
+                    for hgvs, consequence in consequences.items():
+                        mapped_variant = variant_map[hgvs]
+                        if consequence:
+                            mapped_variant.vep_functional_consequence = consequence
+                            mapped_variant.vep_access_date = date.today()
+                            db.add(mapped_variant)
+                        else:
+                            logger.warning(f"Could not retrieve functional consequence for HGVS {hgvs}.")
                     db.commit()
-                else:
-                    logger.warning(f"Could not retrieve functional consequence for HGVS {hgvs_string}.")
+                    queue.clear()
+                    variant_map.clear()
+
+            # Process any remaining variants in the queue
+            if queue:
+                consequences = get_functional_consequence(queue)
+                for hgvs, consequence in consequences.items():
+                    mapped_variant = variant_map[hgvs]
+                    if consequence:
+                        mapped_variant.vep_functional_consequence = consequence
+                        mapped_variant.vep_access_date = date.today()
+                        db.add(mapped_variant)
+                    else:
+                        logger.warning(f"Could not retrieve functional consequence for HGVS {hgvs}.")
+                db.commit()
 
         except Exception as e:
             logger.error(
