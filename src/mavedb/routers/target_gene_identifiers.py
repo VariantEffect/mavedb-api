f418f9e7feb886a23e44cb066a68852ad2646330
Ben Capodanno <capodb@uw.edu>
2025-10-28 14:19:38 -0700

    Enhance API Documentation: fix response status code for missing search text in target gene identifiers search and add summary text

--- src/mavedb/routers/target_gene_identifiers.py
+++ src/mavedb/routers/target_gene_identifiers.py
@@ -12,11 +12,19 @@ from mavedb.view_models.search import TextSearch
 router = APIRouter(
     prefix="/api/v1/target-gene-identifiers",
     tags=["Target Gene Identifiers"],
-    responses={404: {"description": "Not found"}},
+    responses={404: {"description": "Not found"}, 500: {"description": "Internal server error"}},
 )
 
 
-@router.post("/search", status_code=200, response_model=List[external_gene_identifier.ExternalGeneIdentifier])
+@router.post(
+    "/search",
+    status_code=200,
+    response_model=List[external_gene_identifier.ExternalGeneIdentifier],
+    summary="Search target gene identifiers",
+    responses={
+        400: {"description": "Bad request"},
+    },
+)
 def search_target_gene_identifiers(db_name: str, search: TextSearch, db: Session = Depends(deps.get_db)) -> Any:
     """
     Search target gene identifiers.
@@ -36,7 +44,7 @@ def search_target_gene_identifiers(db_name: str, search: TextSearch, db: Session
         lower_search_text = search.text.strip().lower()
         query = query.filter(func.lower(identifier_class.identifier).contains(lower_search_text))
     else:
-        raise HTTPException(status_code=500, detail="Search text is required")
+        raise HTTPException(status_code=400, detail="Search text is required")
 
     items = query.order_by(identifier_class.identifier).limit(50).all()
     if not items:
