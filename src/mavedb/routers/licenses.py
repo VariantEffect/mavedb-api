3cbaeea4fec70bb7c2067e1fad82efd183518483
Ben Capodanno <capodb@uw.edu>
2025-10-28 11:43:35 -0700

    Enhance API documentation: Improve summaries for licenses endpoints

--- src/mavedb/routers/licenses.py
+++ src/mavedb/routers/licenses.py
@@ -7,36 +7,43 @@ from mavedb import deps
 from mavedb.models.license import License
 from mavedb.view_models import license
 
-router = APIRouter(prefix="/api/v1/licenses", tags=["Licenses"], responses={404: {"description": "Not found"}})
+router = APIRouter(
+    prefix="/api/v1/licenses",
+    tags=["Licenses"],
+    responses={
+        404: {"description": "Not found"},
+        500: {"description": "Internal server error"},
+    },
+)
 
 
-@router.get("/", status_code=200, response_model=List[license.ShortLicense], responses={404: {}})
+@router.get("/", status_code=200, response_model=List[license.ShortLicense], summary="List all licenses")
 def list_licenses(
     *,
     db: Session = Depends(deps.get_db),
 ) -> Any:
     """
-    List licenses.
+    List all supported licenses.
     """
 
     items = db.query(License).order_by(License.short_name).all()
     return items
 
 
-@router.get("/active", status_code=200, response_model=List[license.ShortLicense], responses={404: {}})
+@router.get("/active", status_code=200, response_model=List[license.ShortLicense], summary="List active licenses")
 def list_active_licenses(
     *,
     db: Session = Depends(deps.get_db),
 ) -> Any:
     """
-    List active licenses.
+    List all active licenses.
     """
 
     items = db.query(License).where(License.active.is_(True)).order_by(License.short_name).all()
     return items
 
 
-@router.get("/{item_id}", status_code=200, response_model=license.License, responses={404: {}})
+@router.get("/{item_id}", status_code=200, response_model=license.License, summary="Fetch license by ID")
 def fetch_license(
     *,
     item_id: int,
