3ca697ac35a1fadf7762dac4c7eeeab1586bcbd0
Benjamin Capodanno <bencap@outlook.com>
2026-01-28 11:05:05 -0800

    feat: add context manager for database session management

--- src/mavedb/db/session.py
+++ src/mavedb/db/session.py
@@ -1,4 +1,5 @@
 import os
+from contextlib import contextmanager
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -15,8 +16,23 @@ DB_URL = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_DATA
 
 engine = create_engine(
     # For PostgreSQL:
-    DB_URL
+    DB_URL,
+    pool_size=10,
     # For SQLite:
     # DB_URL, connect_args={"check_same_thread": False}
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+@contextmanager
+def db_session():
+    """Provide a transactional scope around a series of operations."""
+    session = SessionLocal()
+    try:
+        yield session
+        session.commit()
+    except Exception:
+        session.rollback()
+        raise
+    finally:
+        session.close()
