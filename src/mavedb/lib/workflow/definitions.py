ecaf1f0d4296a2fdf6cb629c1dff7c90ac0939a7
Benjamin Capodanno <bencap@outlook.com>
2026-02-04 12:41:40 -0800

    feat: add multiple refresh job definitions for ClinVar controls with year and month parameters

--- src/mavedb/lib/workflow/definitions.py
+++ src/mavedb/lib/workflow/definitions.py
@@ -49,6 +49,151 @@ def annotation_pipeline_job_definitions() -> list[JobDefinition]:
             },
             "dependencies": [("submit_uniprot_mapping_jobs_for_score_set", DependencyType.SUCCESS_REQUIRED)],
         },
+        # TODO#650: Simplify or automate the generation of these repetitive job definitions
+        {
+            "key": "refresh_clinvar_controls_201502",
+            "function": "refresh_clinvar_controls",
+            "type": JobType.MAPPED_VARIANT_ANNOTATION,
+            "params": {
+                "correlation_id": None,  # Required param to be filled in at runtime
+                "score_set_id": None,  # Required param to be filled in at runtime
+                "year": 2015,
+                "month": 2,
+            },
+            "dependencies": [("submit_score_set_mappings_to_car", DependencyType.SUCCESS_REQUIRED)],
+        },
+        {
+            "key": "refresh_clinvar_controls_201601",
+            "function": "refresh_clinvar_controls",
+            "type": JobType.MAPPED_VARIANT_ANNOTATION,
+            "params": {
+                "correlation_id": None,  # Required param to be filled in at runtime
+                "score_set_id": None,  # Required param to be filled in at runtime
+                "year": 2016,
+                "month": 1,
+            },
+            "dependencies": [("submit_score_set_mappings_to_car", DependencyType.SUCCESS_REQUIRED)],
+        },
+        {
+            "key": "refresh_clinvar_controls_201701",
+            "function": "refresh_clinvar_controls",
+            "type": JobType.MAPPED_VARIANT_ANNOTATION,
+            "params": {
+                "correlation_id": None,  # Required param to be filled in at runtime
+                "score_set_id": None,  # Required param to be filled in at runtime
+                "year": 2017,
+                "month": 1,
+            },
+            "dependencies": [("submit_score_set_mappings_to_car", DependencyType.SUCCESS_REQUIRED)],
+        },
+        {
+            "key": "refresh_clinvar_controls_201801",
+            "function": "refresh_clinvar_controls",
+            "type": JobType.MAPPED_VARIANT_ANNOTATION,
+            "params": {
+                "correlation_id": None,  # Required param to be filled in at runtime
+                "score_set_id": None,  # Required param to be filled in at runtime
+                "year": 2018,
+                "month": 1,
+            },
+            "dependencies": [("submit_score_set_mappings_to_car", DependencyType.SUCCESS_REQUIRED)],
+        },
+        {
+            "key": "refresh_clinvar_controls_201901",
+            "function": "refresh_clinvar_controls",
+            "type": JobType.MAPPED_VARIANT_ANNOTATION,
+            "params": {
+                "correlation_id": None,  # Required param to be filled in at runtime
+                "score_set_id": None,  # Required param to be filled in at runtime
+                "year": 2019,
+                "month": 1,
+            },
+            "dependencies": [("submit_score_set_mappings_to_car", DependencyType.SUCCESS_REQUIRED)],
+        },
+        {
+            "key": "refresh_clinvar_controls_202001",
+            "function": "refresh_clinvar_controls",
+            "type": JobType.MAPPED_VARIANT_ANNOTATION,
+            "params": {
+                "correlation_id": None,  # Required param to be filled in at runtime
+                "score_set_id": None,  # Required param to be filled in at runtime
+                "year": 2020,
+                "month": 1,
+            },
+            "dependencies": [("submit_score_set_mappings_to_car", DependencyType.SUCCESS_REQUIRED)],
+        },
+        {
+            "key": "refresh_clinvar_controls_202101",
+            "function": "refresh_clinvar_controls",
+            "type": JobType.MAPPED_VARIANT_ANNOTATION,
+            "params": {
+                "correlation_id": None,  # Required param to be filled in at runtime
+                "score_set_id": None,  # Required param to be filled in at runtime
+                "year": 2021,
+                "month": 1,
+            },
+            "dependencies": [("submit_score_set_mappings_to_car", DependencyType.SUCCESS_REQUIRED)],
+        },
+        {
+            "key": "refresh_clinvar_controls_202201",
+            "function": "refresh_clinvar_controls",
+            "type": JobType.MAPPED_VARIANT_ANNOTATION,
+            "params": {
+                "correlation_id": None,  # Required param to be filled in at runtime
+                "score_set_id": None,  # Required param to be filled in at runtime
+                "year": 2022,
+                "month": 1,
+            },
+            "dependencies": [("submit_score_set_mappings_to_car", DependencyType.SUCCESS_REQUIRED)],
+        },
+        {
+            "key": "refresh_clinvar_controls_202301",
+            "function": "refresh_clinvar_controls",
+            "type": JobType.MAPPED_VARIANT_ANNOTATION,
+            "params": {
+                "correlation_id": None,  # Required param to be filled in at runtime
+                "score_set_id": None,  # Required param to be filled in at runtime
+                "year": 2023,
+                "month": 1,
+            },
+            "dependencies": [("submit_score_set_mappings_to_car", DependencyType.SUCCESS_REQUIRED)],
+        },
+        {
+            "key": "refresh_clinvar_controls_202401",
+            "function": "refresh_clinvar_controls",
+            "type": JobType.MAPPED_VARIANT_ANNOTATION,
+            "params": {
+                "correlation_id": None,  # Required param to be filled in at runtime
+                "score_set_id": None,  # Required param to be filled in at runtime
+                "year": 2024,
+                "month": 1,
+            },
+            "dependencies": [("submit_score_set_mappings_to_car", DependencyType.SUCCESS_REQUIRED)],
+        },
+        {
+            "key": "refresh_clinvar_controls_202501",
+            "function": "refresh_clinvar_controls",
+            "type": JobType.MAPPED_VARIANT_ANNOTATION,
+            "params": {
+                "correlation_id": None,  # Required param to be filled in at runtime
+                "score_set_id": None,  # Required param to be filled in at runtime
+                "year": 2025,
+                "month": 1,
+            },
+            "dependencies": [("submit_score_set_mappings_to_car", DependencyType.SUCCESS_REQUIRED)],
+        },
+        {
+            "key": "refresh_clinvar_controls_202601",
+            "function": "refresh_clinvar_controls",
+            "type": JobType.MAPPED_VARIANT_ANNOTATION,
+            "params": {
+                "correlation_id": None,  # Required param to be filled in at runtime
+                "score_set_id": None,  # Required param to be filled in at runtime
+                "year": 2026,
+                "month": 1,
+            },
+            "dependencies": [("submit_score_set_mappings_to_car", DependencyType.SUCCESS_REQUIRED)],
+        },
     ]
 
 
