8c4ab61e1efc8a7d5389332aa514b3134f50d2bc
Sally Grindstaff <sallybg@uw.edu>
2026-04-15 15:06:07 -0700

    Add annotation types for new post-mapping annotations

--- src/mavedb/models/enums/annotation_type.py
+++ src/mavedb/models/enums/annotation_type.py
@@ -10,3 +10,5 @@ class AnnotationType(str, Enum):
     CLINVAR_CONTROL = "clinvar_control"
     VEP_FUNCTIONAL_CONSEQUENCE = "vep_functional_consequence"
     LDH_SUBMISSION = "ldh_submission"
+    HGVS = "hgvs"
+    VEP_FUNCTIONAL_CONSEQUENCE = "vep_functional_consequence"
