a66674d33292f249c55eda4b2decbe051feaa6f7
Benjamin Capodanno <bencap@outlook.com>
2026-05-08 09:23:48 -0700

    bump version to 2026.2.0

--- src/mavedb/__init__.py
+++ src/mavedb/__init__.py
@@ -6,7 +6,7 @@ application_logging.configure()
 logger = module_logging.getLogger(__name__)
 
 __project__ = "mavedb-api"
-__version__ = "2026.1.2"
+__version__ = "2026.2.0"
 
 logger.info(f"MaveDB {__version__}")
 
